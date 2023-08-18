50fa3e3a18a2ba19a891a04bf8c03303b6980064
SinghR7 <SinghR7@mskcc.org>
2023-08-17 17:17:53 -0700

    add minio support

--- cedars/app/database.py
+++ cedars/app/database.py
@@ -1,5 +1,19 @@
 """Initialize database connection."""
 from flask_pymongo import PyMongo
+from minio import Minio
+from dotenv import dotenv_values
 
-
+config = dotenv_values(".env")
 mongo = PyMongo()
+
+client = Minio(
+    f'{config["MINIO_HOST"]}:{config["MINIO_PORT"]}',
+    access_key=config["MINIO_ACCESS_KEY"],
+    secret_key=config["MINIO_SECRET_KEY"],
+    secure=False # should be true for prod or AWS
+)
+
+if not client.bucket_exists("cedars"):
+    client.make_bucket("cedars")
+else:
+    print("Bucket 'cedars' already exists")
