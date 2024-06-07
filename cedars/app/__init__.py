04948d6583eb081d0c0b4e7e7f53f40d113faff5
ygivenx <singhrohan@outlook.com>
2024-06-07 14:19:08 -0700

    add download and rq status

--- cedars/app/__init__.py
+++ cedars/app/__init__.py
@@ -25,7 +25,7 @@ def rq_init_app(cedars_rq):
     cedars_rq.ops_queue = rq.Queue(cedars_rq.config["RQ"]['ops_queue_name'],
                                    connection=cedars_rq.redis,
                                    default_timeout=cedars_rq.config["RQ"]['job_timeout'])
-    
+
     cedars_rq.extensions['rq'] = cedars_rq
     return cedars_rq
 
