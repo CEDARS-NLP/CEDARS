1b32f410600e75a5790d254c896e916864d8f7f9
Kayan1920 <K4y4n12345@gmail.com>
2025-07-25 18:34:21 +0530

    Changes to align with main.

--- cedars/gunicorn.conf.py
+++ cedars/gunicorn.conf.py
@@ -1,7 +1,5 @@
 import multiprocessing as mp
-
-from prometheus_flask_exporter.multiprocess import GunicornPrometheusMetrics
-import os
+from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
 
 workers = 4
 threads = 4
@@ -13,10 +11,5 @@ preload_app = False
 disable_redirect_access_to_syslog = True
 accesslog = "/dev/null"
 
-def when_ready(server):
-    os.environ["PROMETHEUS_MULTIPROC_DIR"] = "tmp/prometheus"
-    GunicornPrometheusMetrics.start_http_server_when_ready(2001)
-
-
 def child_exit(server, worker):
-    GunicornPrometheusMetrics.mark_process_dead_on_child_exit(worker.pid)
+    GunicornInternalPrometheusMetrics.mark_process_dead_on_child_exit(worker.pid)
