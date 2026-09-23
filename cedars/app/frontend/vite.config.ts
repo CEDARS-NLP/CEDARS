32d69334ca8bd237c336fd0fefd06663fbb4d5a7
Irani <iranik@mskcc.org>
2026-09-23 20:09:31 +0530

    Updated config to work with tests.

--- cedars/app/frontend/vite.config.ts
+++ cedars/app/frontend/vite.config.ts
@@ -1,5 +1,5 @@
 import path from "path"
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
